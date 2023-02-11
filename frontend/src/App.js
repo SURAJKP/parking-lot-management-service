ca42d0ff0e03af5b929c088b256723505b496d19
SURAJKP <surajkpatil444@gmail.com>
2023-02-11 17:43:13 +0530

    Connected frontend with backend, Api integration

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,20 @@
+import React, { useState, useEffect } from "react";
 import './App.css';
 
 function App() {
+  const [availableSlots, setAvailableSlots] = useState("");
+
+  useEffect(() => {
+    fetch("http://localhost:8000/slots")
+      .then((res) => res.json())
+      .then((data) => setAvailableSlots(data.slots));
+  }, []);
+
   return (
-    <div className="App" data-testid="app">
+    <div className="App">
       <header className="App-header">
         <h1>Parking Lot Managment Service</h1>
+        <h2>{availableSlots}</h2>
       </header>
     </div>
   );
